58858913b9b43b7265e6513fdc74c56ce84ce182
Carlos Marques <fc51964@alunos.fc.ul.pt>
2020-04-25 23:03:54 +0100

    Criei o app-routing module para dizer que componente aparece em cada url

--- angular-projeto-psi/src/app/app-routing.module.ts
+++ angular-projeto-psi/src/app/app-routing.module.ts
@@ -2,7 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HotelListComponent },
+  { path: ':hotelName', component: HotelDetailsComponent }
+  { path: ':hotelName/quartos', component: HotelRoomListComponent },
+  { path: ':hotelName/quartos/:tipoQuarto', component: HotelRoomDetailsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
