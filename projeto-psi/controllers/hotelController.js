2512cfc3cfe132c5cb67dbe2b89ca0259498689e
Guilherme Teixeira <fc49021@alunos.fc.ul.pt>
2020-04-27 21:56:00 +0100

    getHotels e getHotel na teoria feitos (sem teste), mas ainda tenho que mudar, depende do Schema

---
+++ projeto-psi/controllers/hotelController.js
@@ -0,0 +1,23 @@
+var Hotel = require('../models/hotel')
+var async = require('async')
+
+const { body, validationResult } = require('express-validator/check');
+const { sanitizeBody } = require('express-validator/filter');
+
+exports.get_hotels = function(req, res, next) {
+  Hotel.find()
+      .sort([['hotel_name', 'ascending']])
+      .exec(function (err, list_hotels) {
+          if (err) { return next(err); }
+          // Successful, so render.
+          res.json({hotel_list: list_hotels });
+      })
+}
+
+exports.get_hotel = function(req, res, next) {
+  Hotel.findById(req.params.id)
+    .exec(function (err, hotel) {
+      if (err) { return next(err); }
+      res.json({hotel : hotel})
+    }
+}
