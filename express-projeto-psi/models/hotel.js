b8f7a0f0bc4b6d0c0a8322ed4ed02d15a82eb400
Gabriel Freitas <fc51035@alunos.fc.ul.pt>
2020-04-28 21:12:17 +0100

    create schema hotel concluido

---
+++ express-projeto-psi/models/hotel.js
@@ -0,0 +1,27 @@
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+var HotelSchema = new Schema(
+  {
+    id: {type: String, required: true, max: 10},
+    name: {type: String, required: true, max: 20},
+    description: {type: String, required: true},
+    address: {type: String, required: true, max: 100},
+    coordinates: {type: String, required: true, max: 100},
+    phone_number: {type: String, required: true, max: 15},
+    mail: {type: String, required: true, max: 20},
+    image: [{type: String}],
+    servico: [{type: Schema.Types.ObjectId, ref: 'Servico'}]
+  }
+);
+
+// Virtual for hotel's URL
+HotelSchema
+.virtual('url')
+.get(function () {
+  return '/catalog/hotel/' + this._id;
+});
+
+//Export model
+module.exports = mongoose.model('Hotel', HotelSchema);
