a144754e0c7dd9b715ef56694a1706bb59f307d4
Carlos Marques <fc51964@alunos.fc.ul.pt>
2020-05-02 13:46:16 +0100

    Fiz os arrays a mao

--- express-projeto-psi/populatedb.js
+++ express-projeto-psi/populatedb.js
@@ -248,48 +248,16 @@ function createHoteis(cb) {
     cb);
 }
 
-var tresQuartos = []
-var cincoQuartos = []
-var centoOitentaDoisQuartos = []
-var quinzeQuartos = []
-var centoCatorzeQuartos = []
-var noventaOitoQuartos = []
-var oitoQuartos = [];
-
-function mudarLength(array, n, cb) {
-  for (var i = 0; i < n, i++;) {
-    array.push[i];
-  }
-}
-
-var hoteisOrdenados = [];
-
-function preencherArrays(cb) {
-  async.series([
-    function (callback) {
-      mudarLength(tresQuartos, 3, callback);
-    },
-    function (callback) {
-      mudarLength(cincoQuartos, 5, callback);
-    }, function (callback) {
-      mudarLength(centoOitentaDoisQuartos, 182, callback);
-    }, function (callback) {
-      mudarLength(quinzeQuartos, 15, callback);
-    }, function (callback) {
-      mudarLength(centoCatorzeQuartos, 114, callback);
-    }, function (callback) {
-      mudarLength(noventaOitoQuartos, 98, callback);
-    }, function (callback) {
-      mudarLength(oitoQuartos, 8, callback);
-    },
-  ],
-    // optional callback
-    cb);
-}
+var tresQuartos = [0,1,2]
+var cincoQuartos = [0,1,2,3,4]
+var centoOitentaDoisQuartos = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100,101,102,103,104,105,106,107,108,109,110,111,112,113,114,115,116,117,118,119,120,121,122,123,124,125,126,127,128,129,130,131,132,133,134,135,136,137,138,139,140,141,142,143,144,145,146,147,148,149,150,151,152,153,154,155,156,157,158,159,160,161,162,163,164,165,166,167,168,169,170,171,172,173,174,175,176,177,178,179,180,181]
+var quinzeQuartos = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14]
+var centoCatorzeQuartos = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100,101,102,103,104,105,106,107,108,109,110,111,112,113]
+var noventaOitoQuartos = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97]
+var oitoQuartos = [0,1,2,3,4,5,6,7];
 
 function createQuartos(cb) {
   async.series([
-    preencherArrays(cb),
     function (callback) {
       async.map(tresQuartos, function (numero, cb) {
         quartoCreate(hoteis[0], 'Standard', 180, 270, servicosDouroStandard, callback);
