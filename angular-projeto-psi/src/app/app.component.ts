883fca65b9a0a45b913c4bc084225a774f56f777
Gabriel Freitas <fc51035@alunos.fc.ul.pt>
2020-05-13 17:04:58 +0100

    o header já esta no app-component

--- angular-projeto-psi/src/app/app.component.ts
+++ angular-projeto-psi/src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Hotel } from './interfaces/Hotel';
+import { HotelService } from './hotel.service';
+import { DataService } from "./data.service";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-projeto-psi';
+  hotelList: Hotel[];
+  message: string;
+
+  constructor(private hotelService: HotelService,
+              private data: DataService) { }
+
+  ngOnInit(): void {
+    this.getHotels();
+    //this.data.currentId.subscribe(message => this.message = message);
+  }
+
+  private getHotels() {
+    this.hotelService.getHotels().subscribe(response => this.hotelList = response.hotel_list);
+  }
 }
