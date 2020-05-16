8097be12b5ea1e41d19c0640aa17e6ff27632340
tiagofrancisco97 <fc51729@alunos.fc.ul.pt>
2020-05-16 21:41:36 +0100

    correcao no schema da reserva

--- express-projeto-psi/models/reserva.js
+++ express-projeto-psi/models/reserva.js
@@ -14,7 +14,7 @@ var ReservaSchema = new Schema(
         nif: {type: Number, maxlength: 9, minlength: 9, required: true},
         numeroCartao: {type: Number, minlength: 16, maxlength: 16, required: true},
         ccv: {type: Number, minlength: 3, maxlength: 3, required: true},
-        anoValidade: {type:Number, minlength: 4, maxlength: 4, required: true},
+        anoValidade: {type:Number, minlength: 2, maxlength: 4, required: true},
         mesValidade: {type:Number, maxlength: 2, required: true}
     }
 )
