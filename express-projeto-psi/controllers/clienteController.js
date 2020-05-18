f59a86a095b95eaf4f42c5693f894890342f5879
Carlos Marques <fc51964@alunos.fc.ul.pt>
2020-05-18 21:48:38 +0100

    Comentei a verificao no addCliente

--- express-projeto-psi/controllers/clienteController.js
+++ express-projeto-psi/controllers/clienteController.js
@@ -15,7 +15,7 @@ exports.getClientes = function (req, res, next) {
 }
 
 exports.getCliente = function (req, res, next) {
-    Cliente.find({'email' : req.params.email})
+    Cliente.find({ 'email': req.params.email })
         .exec(function (err, cliente) {
             if (err) { return next(err); }
             res.json(cliente);
@@ -28,51 +28,51 @@ exports.cliente_create_get = function (req, res, next) {
 
 exports.cliente_create_post = [
 
-  // Validate fields.
-  body('nome').isLength({ min: 1 }).trim().withMessage('Cliente nome must be specified.'),
-  body('password').isLength({ min: 1 }).trim().withMessage('Cliente password must be specified.'),
-  body('email').isLength({ min: 1 }).trim().withMessage('Cliente email must be specified.'),
+    /* // Validate fields.
+    body('nome').isLength({ min: 1 }).trim().withMessage('Cliente nome must be specified.'),
+    body('password').isLength({ min: 1 }).trim().withMessage('Cliente password must be specified.'),
+    body('email').isLength({ min: 1 }).trim().withMessage('Cliente email must be specified.'),
 
-  // Sanitize fields.
-  sanitizeBody('nome').escape(),
-  sanitizeBody('password').escape(),
-  sanitizeBody('email').escape(),
-  sanitizeBody('morada').escape(),
-  sanitizeBody('numero_telefone').escape(),
-  sanitizeBody('nif').escape(),
+    // Sanitize fields.
+    sanitizeBody('nome').escape(),
+    sanitizeBody('password').escape(),
+    sanitizeBody('email').escape(),
+    sanitizeBody('morada').escape(),
+    sanitizeBody('numero_telefone').escape(),
+    sanitizeBody('nif').escape(), */
 
-  // Process request after validation and sanitization.
-  (req, res, next) => {
+    // Process request after validation and sanitization.
+    (req, res, next) => {
 
-      // Extract the validation errors from a request.
-      const errors = validationResult(req);
+        // Extract the validation errors from a request.
+        const errors = validationResult(req);
 
-      // Create cliente object with escaped and trimmed data
-      var cliente = new Cliente(
-          {
-              nome: req.body.nome,
-              password: req.body.password,
-              email: req.body.email,
-              morada: req.body.morada,
-              numero_telefone: req.body.numero_telefone,
-              nif: req.body.nif,
-          }
-      );
+        // Create cliente object with escaped and trimmed data
+        var cliente = new Cliente(
+            {
+                nome: req.body.nome,
+                password: req.body.password,
+                email: req.body.email,
+                morada: req.body.morada,
+                numero_telefone: req.body.numero_telefone,
+                nif: req.body.nif,
+            }
+        );
 
-      if (!errors.isEmpty()) {
-          // There are errors. Render form again with sanitized values/errors messages.
-          res.json({cliente: cliente, errors: errors.array() });
-          return;
-      }
-      else {
-          // Save cliente.
-          cliente.save(function (err) {
-              if (err) { return next(err); }
-              // Successful - redirect to new Reserva record.
-              res.redirect(cliente.url);
-          });
-      }
-  }
+        if (!errors.isEmpty()) {
+            // There are errors. Render form again with sanitized values/errors messages.
+            res.json({ cliente: cliente, errors: errors.array() });
+            return;
+        }
+        else {
+            // Save cliente.
+            cliente.save(function (err) {
+                if (err) { return next(err); }
+                // Successful - redirect to new Reserva record.
+                res.redirect(cliente.url);
+            });
+        }
+    }
 ];
 
 exports.cliente_update_get = function (req, res, next) {
@@ -85,25 +85,25 @@ exports.cliente_update_get = function (req, res, next) {
             return next(err);
         }
         // Success.
-        res.json({cliente: cliente });
+        res.json({ cliente: cliente });
 
     });
 };
 
 exports.cliente_update_post = [
 
-  // Validate fields.
-  body('nome').isLength({ min: 1 }).trim().withMessage('Cliente nome must be specified.'),
-  body('password').isLength({ min: 1 }).trim().withMessage('Cliente password must be specified.'),
-  body('email').isLength({ min: 1 }).trim().withMessage('Cliente email must be specified.'),
+    // Validate fields.
+    body('nome').isLength({ min: 1 }).trim().withMessage('Cliente nome must be specified.'),
+    body('password').isLength({ min: 1 }).trim().withMessage('Cliente password must be specified.'),
+    body('email').isLength({ min: 1 }).trim().withMessage('Cliente email must be specified.'),
 
-  // Sanitize fields.
-  sanitizeBody('nome').escape(),
-  sanitizeBody('password').escape(),
-  sanitizeBody('email').escape(),
-  sanitizeBody('morada').escape(),
-  sanitizeBody('numero_telefone').escape(),
-  sanitizeBody('nif').escape(),
+    // Sanitize fields.
+    sanitizeBody('nome').escape(),
+    sanitizeBody('password').escape(),
+    sanitizeBody('email').escape(),
+    sanitizeBody('morada').escape(),
+    sanitizeBody('numero_telefone').escape(),
+    sanitizeBody('nif').escape(),
 
     // Process request after validation and sanitization.
     (req, res, next) => {
@@ -125,7 +125,7 @@ exports.cliente_update_post = [
 
         if (!errors.isEmpty()) {
             // There are errors. Render the form again with sanitized values and error messages.
-            res.json({cliente: cliente, errors: errors.array() });
+            res.json({ cliente: cliente, errors: errors.array() });
             return;
         }
         else {
