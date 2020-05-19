fc5f1a28986706369f338da714ccbf58bbbd9ce3
Guilherme Teixeira <fc49021@alunos.fc.ul.pt>
2020-05-19 21:37:43 +0100

    console.log depois do save

--- express-projeto-psi/controllers/clienteController.js
+++ express-projeto-psi/controllers/clienteController.js
@@ -58,7 +58,6 @@ exports.cliente_create_post = [
               nif: req.body.nif,
           }
       );
-      console.log(cliente.nome);
       if (!errors.isEmpty()) {
           // There are errors. Render form again with sanitized values/errors messages.
           res.json({cliente: cliente, errors: errors.array() });
@@ -67,6 +66,7 @@ exports.cliente_create_post = [
       else {
           // Save cliente.
           cliente.save(function (err) {
+            console.log("FOI SALVO")
               if (err) { return next(err); }
               // Successful - redirect to new Cliente record.
               res.redirect(cliente.url);
