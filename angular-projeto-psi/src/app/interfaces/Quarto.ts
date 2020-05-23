25d1b846db5c775cab79bca208794b5cb5863600
CCaritas <carlos@DESKTOP-RT6SHIR.localdomain>
2020-05-23 11:14:08 +0100

    criado o user-area component

--- angular-projeto-psi/src/app/interfaces/Quarto.ts
+++ angular-projeto-psi/src/app/interfaces/Quarto.ts
@@ -1,7 +1,7 @@
 import {Hotel} from './Hotel';
 import {TipoQuarto} from './TipoQuarto';
 export interface Quarto {
-  id: string;
+  _id: string;
   hotel: Hotel;
   tipoQuarto: TipoQuarto;
   precoBaixo: number;
