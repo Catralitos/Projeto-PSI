26c5dd93bd9c4721f224f2cef19e568d25366a81
Guilherme Teixeira <fc49021@alunos.fc.ul.pt>
2020-05-25 16:17:00 +0100

    Acredito que o commit final

--- express-projeto-psi/controllers/reservaController.js
+++ express-projeto-psi/controllers/reservaController.js
@@ -25,37 +25,6 @@ exports.getReservas = function (req, res, next) {
 }
 
 exports.reserva_create_post = [
-    /*
-     check('quarto').escape(),
-     check('nome').isLength({ min: 3}).trim().escape(),
-     check('email').isEmail().normalizeEmail(),
-     check('checkin').toDate().escape(),
-     check('checkout').toDate().escape(),
-     check('morada').isLength({ min: 3}).trim().escape(),
-     check('numero_telefone').isLength({ min: 13, max: 14}).trim().escape(),
-     check('nif').isNumeric().trim().escape(),
-     check('numeroCartao').isNumeric().trim().escape(),
-     check('mesValidade').isNumeric().trim().escape(),
-     check('anoValidade').isNumeric().trim().escape(),
-     check('ccv').isNumeric().trim().escape(),
-     */
-    // Validate fields.
-    /*
-    body('nome').isLength({ min: 1 }).trim().withMessage('Cliente nome must be specified.'),
-        //.isAlphanumeric().withMessage('Cliente nome has non-alphanumeric characters.'),
-    body('email').isLength({ min: 1 }).trim().withMessage('Cliente email must be specified.'),
-        //.isAlphanumeric().withMessage('Cliente email has non-alphanumeric characters.'),
-    body('numero_telefone').isLength({ min: 1 }).trim().withMessage('Cliente numero_telefone must be specified.'),
-    body('morada').isLength({ min: 1 }).trim().withMessage('Cliente morada must be specified.'),
-        //.isAlphanumeric().withMessage('Cliente morada has non-alphanumeric characters.'),
-    body('checkin', 'Invalid checkin').optional({ checkFalsy: true }).isISO8601(),
-    body('checkout', 'Invalid checkout').optional({ checkFalsy: true }).isISO8601(),
-    body('nif').isLength({ min: 1 }).withMessage('Cliente nif must be specified.'),
-    body('anoValidade').isLength({ min: 1 }).withMessage('Cliente anoValidade must be specified.'),
-    body('mesValidade').isLength({ min: 1 }).withMessage('Cliente mesValidade must be specified.'),
-    body('numeroCartao').isLength({ min: 1 }).withMessage('Cliente numeroCartao must be specified.'),
-    body('ccv').isLength({ min: 1 }).withMessage('Cliente ccv must be specified.'),
-*/
     body('checkin').isISO8601().toDate(),
     body('checkout').isISO8601().toDate(),
     body('nome').isLength({ min: 2 }).escape(),
@@ -63,7 +32,7 @@ exports.reserva_create_post = [
     body('numero_telefone').trim(),
     body('email').isEmail().trim().normalizeEmail(),
     body('nif').isLength({ min: 8 }).isNumeric().trim(),
-    body('numeroCartao').isCreditCard().trim(),
+    body('numeroCartao').isLength({min:15}).isLength({max:16}).trim(),
     body('ccv').isLength({ min: 2}).isLength({ max: 3}).isNumeric().trim(),
     body('anoValidade').isLength({ min: 2 }).isLength({ max: 4 }).isNumeric().trim(),
     body('mesValidade').isLength({ min: 1 }).isLength({ max: 2 }).isNumeric().trim(),
